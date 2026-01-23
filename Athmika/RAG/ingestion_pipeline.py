363d7efa2c72fa33cdc4e9dee9d7586d3dd5ce65
AthmikaNovintix <athmika.premanandhan@novintix.com>
2026-01-23 12:23:50 +0530

    Overcame the deprecated packages
    I used "langchain_huggingface" instead of langchian.embeddings to utilise the Sentence Transformer model for embedding

--- Athmika/RAG/ingestion_pipeline.py
+++ Athmika/RAG/ingestion_pipeline.py
@@ -1,7 +1,7 @@
 import os
 from langchain_community.document_loaders import TextLoader, DirectoryLoader
 from langchain_text_splitters import CharacterTextSplitter
-from langchain.embeddings import HuggingFaceEmbeddings
+from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_chroma import Chroma
 from dotenv import load_dotenv
 
